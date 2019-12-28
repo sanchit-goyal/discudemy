bc4c1c70287a427bd17d897dfec85899ef52b3dd
Sanchit Goyal <sanchit.goyal@orange.com>
2019-12-28 19:02:24 +0530

    messing with protocol buffer

--- IO.py
+++ IO.py
@@ -2,11 +2,14 @@ import csv
 import json
 import pickle
 
+from proto.course_pb2 import Courses
+
 __FILE_ENCODING_UTF_8__ = 'utf-8'
 __FILE_ENCODING_UTF_16__ = 'utf-16'
 __OBJ_FILE_NAME__ = 'files/discudemy.obj'
 __JSON_FILE_NAME__ = 'files/discudemy.json'
 __CSV_FILE_NAME__ = 'files/discudemy.csv'
+__PROTO_FILE_NAME__ = 'files/discudemy.proto.obj'
 
 
 def write_to_csv_file(_data):
@@ -34,3 +37,15 @@ def serialize_to_file(data_to_serialize):
 def deserialize_from_file():
     with open(__OBJ_FILE_NAME__, 'rb') as file:
         return pickle.load(file)
+
+
+def write_proto_to_file(courses: Courses):
+    with open(__PROTO_FILE_NAME__, 'wb') as file:
+        file.write(courses.SerializeToString())
+
+
+def read_proto_from_file() -> Courses:
+    courses = Courses()
+    with open(__PROTO_FILE_NAME__, 'rb') as file:
+        courses.ParseFromString(file.read())
+    return courses
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+import os
+
 import IO
 from DiscUdemy import DiscUdemy
+from proto.course_pb2 import Courses
 from scraper import for_page_threading
-
-
-def get_obj_repr_from_set(_data):
-    classes = []
-    for _ in _data:
-        classes.append(repr(DiscUdemy(_)))
-    return classes
 
 
 def fix_files():
@@ -19,11 +15,11 @@ def fix_files():
     IO.serialize_to_file(_data)
     IO.write_to_json_file(_data)
     IO.write_to_csv_file(get_obj_from_set(_data))
+    IO.write_proto_to_file(get_proto_from_set(_data))
 
 
 def get_obj_from_set(_data):
     """
-
     :param _data:
     :return: DiscUdemy
     """
@@ -33,10 +29,32 @@ def get_obj_from_set(_data):
     return disc_udemy_list
 
 
+def get_proto_from_set(_data) -> Courses:
+    courses = Courses()
+    for _ in _data:
+        proto_course = courses.courses.add()
+        proto_course.name = _.name
+        proto_course.url = _.url
+    return courses
+
+
 __SEARCH_KEY__ = 'JAVA'
 if __name__ == '__main__':
     # fix_files()
     data = get_obj_from_set(IO.deserialize_from_file())
-    for _ in data:
-        if _.name.upper().find(__SEARCH_KEY__) > -1:
-            print(_)
+
+    IO.write_proto_to_file(get_proto_from_set(data))
+
+    print('proto file size =', os.stat('files/discudemy.proto.obj').st_size)
+    print('pickle file size =', os.stat('files/discudemy.obj').st_size)
+    print('json file size =', os.stat('files/discudemy.json').st_size)
+    print('csv file size =', os.stat('files/discudemy.csv').st_size)
+
+    data = IO.read_proto_from_file()
+    print(data.courses[0])
+
+    # test.ParseFromString(data_2[0].SerializeToString())
+
+    # for _ in data:
+    #     if _.name.upper().find(__SEARCH_KEY__) > -1:
+    #         print(_)
